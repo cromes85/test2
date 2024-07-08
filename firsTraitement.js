132d6a28345c123418e4938e17f6cc5a70aece08
Cromes85 <idv230685@gmail.com>
2024-07-08 13:57:40 +0200

    gg

--- firsTraitement.js
+++ firsTraitement.js
@@ -1,18 +1,13 @@
 function processImage(img) {
-  const canvas = document.getElementById("imageCanvas");
-  const ctx = canvas.getContext("2d");
-  canvas.width = img.width;
-  canvas.height = img.height;
-  ctx.drawImage(img, 0, 0);
+    const canvas = document.getElementById('imageCanvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
 
-  // En supposant que l'ID est toujours dans la section inférieure de la carte, ajustez si nécessaire
-  const bottomSectionHeight = img.height * 0.1; // ajustez le pourcentage selon les besoins
-  const imageData = ctx.getImageData(
-    0,
-    img.height - bottomSectionHeight,
-    img.width,
-    bottomSectionHeight
-  );
+    // En supposant que l'ID est toujours dans la section inférieure de la carte, ajustez si nécessaire
+    const bottomSectionHeight = img.height * 0.1;  // ajustez le pourcentage selon les besoins
+    const imageData = ctx.getImageData(0, img.height - bottomSectionHeight, img.width, bottomSectionHeight);
 
-  detectId(imageData);
+    detectId(imageData);
 }
