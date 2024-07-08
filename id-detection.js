85ba67cb5025cc3ab6f897c4c47a87f5ba03fd97
Cromes85 <idv230685@gmail.com>
2024-07-08 15:35:53 +0200

    ii

--- id-detection.js
+++ id-detection.js
@@ -1,24 +1,26 @@
 function detectId(canvas) {
   Tesseract.recognize(
-      canvas.toDataURL(),  // Utilisation de l'URL de données de l'image
-      'eng',
-      {
-          logger: m => console.log(m)
-      }
-  ).then(({ data: { text } }) => {
+    canvas.toDataURL(), // Utilisation de l'URL de données de l'image
+    "eng",
+    {
+      logger: (m) => console.log(m),
+    }
+  )
+    .then(({ data: { text } }) => {
       const idMatch = text.match(/\b\d{1,3}\/\d{1,3}\b/);
       if (idMatch) {
-          displayResult(idMatch[0]);
+        displayResult(idMatch[0]);
       } else {
-          displayResult('ID non trouvé');
+        displayResult("ID non trouvé");
       }
-  }).catch(err => {
+    })
+    .catch((err) => {
       console.error(err);
-      displayResult('Erreur lors de la reconnaissance de l\'ID');
-  });
+      displayResult("Erreur lors de la reconnaissance de l'ID");
+    });
 }
 
 function displayResult(id) {
-  const resultsDiv = document.getElementById('results');
+  const resultsDiv = document.getElementById("results");
   resultsDiv.innerHTML = `<p>ID détecté : ${id}</p>`;
 }
