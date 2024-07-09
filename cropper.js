68d7866fed38085a00df6800f682178361133ca5
Cromes85 <idv230685@gmail.com>
2024-07-09 10:26:56 +0200

    new file cropper

---
+++ cropper.js
@@ -0,0 +1,23 @@
+let cropper;
+
+function initializeCropper(img) {
+  if (cropper) {
+    cropper.destroy();
+  }
+
+  cropper = new Cropper(img, {
+    aspectRatio: NaN,
+    viewMode: 1,
+    autoCropArea: 0.5,
+    movable: true,
+    zoomable: true,
+    rotatable: true,
+    scalable: true,
+  });
+
+  document.getElementById("cropButton").style.display = "inline-block";
+}
+
+function getCroppedCanvas() {
+  return cropper.getCroppedCanvas();
+}
--- main.js
+++ main.js
@@ -1,36 +1,23 @@
-let cropper;
-document.getElementById('imageUpload').addEventListener('change', handleImageUpload);
+document
+  .getElementById("imageUpload")
+  .addEventListener("change", handleImageUpload);
 
 function handleImageUpload(event) {
-    const file = event.target.files[0];
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            const img = document.getElementById('image');
-            img.src = e.target.result;
-            img.style.display = 'block';
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const img = document.getElementById("image");
+      img.src = e.target.result;
+      img.style.display = "block";
 
-            if (cropper) {
-                cropper.destroy();
-            }
-
-            cropper = new Cropper(img, {
-                aspectRatio: NaN,
-                viewMode: 1,
-                autoCropArea: 1,
-                movable: true,
-                zoomable: true,
-                rotatable: true,
-                scalable: true,
-            });
-
-            document.getElementById('cropButton').style.display = 'inline-block';
-        }
-        reader.readAsDataURL(file);
-    }
+      initializeCropper(img);
+    };
+    reader.readAsDataURL(file);
+  }
 }
 
-document.getElementById('cropButton').addEventListener('click', function () {
-    const canvas = cropper.getCroppedCanvas();
-    processImage(canvas);
+document.getElementById("cropButton").addEventListener("click", function () {
+  const croppedCanvas = getCroppedCanvas();
+  processImage(croppedCanvas);
 });
