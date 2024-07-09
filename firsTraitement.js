e9f98e0422049efbd7f289d93df624d2523b29ad
Cromes85 <idv230685@gmail.com>
2024-07-09 10:40:39 +0200

    next

--- firsTraitement.js
+++ firsTraitement.js
@@ -1,6 +1,6 @@
 function processImage(croppedCanvas) {
-  const canvas = document.getElementById('imageCanvas');
-  const ctx = canvas.getContext('2d');
+  const canvas = document.getElementById("imageCanvas");
+  const ctx = canvas.getContext("2d");
   const scale = 2; // Augmentez ce facteur pour une résolution plus élevée
   canvas.width = croppedCanvas.width * scale;
   canvas.height = croppedCanvas.height * scale;
@@ -10,12 +10,24 @@ function processImage(croppedCanvas) {
   const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
   const data = imageData.data;
   for (let i = 0; i < data.length; i += 4) {
-      const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
-      data[i] = avg > 128 ? 255 : 0;
-      data[i + 1] = avg > 128 ? 255 : 0;
-      data[i + 2] = avg > 128 ? 255 : 0;
+    const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+    data[i] = avg > 128 ? 255 : 0;
+    data[i + 1] = avg > 128 ? 255 : 0;
+    data[i + 2] = avg > 128 ? 255 : 0;
   }
   ctx.putImageData(imageData, 0, 0);
+
+  // Conversion en niveaux de gris
+  const grayImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const grayData = grayImageData.data;
+  for (let i = 0; i < grayData.length; i += 4) {
+    const gray =
+      0.299 * grayData[i] + 0.587 * grayData[i + 1] + 0.114 * grayData[i + 2];
+    grayData[i] = gray;
+    grayData[i + 1] = gray;
+    grayData[i + 2] = gray;
+  }
+  ctx.putImageData(grayImageData, 0, 0);
 
   detectId(canvas);
 }
--- main.js
+++ main.js
@@ -1,23 +1,21 @@
-document
-  .getElementById("imageUpload")
-  .addEventListener("change", handleImageUpload);
+document.getElementById('imageUpload').addEventListener('change', handleImageUpload);
 
 function handleImageUpload(event) {
-  const file = event.target.files[0];
-  if (file) {
-    const reader = new FileReader();
-    reader.onload = function (e) {
-      const img = document.getElementById("image");
-      img.src = e.target.result;
-      img.style.display = "block";
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const img = document.getElementById('image');
+            img.src = e.target.result;
+            img.style.display = 'block';
 
-      initializeCropper(img);
-    };
-    reader.readAsDataURL(file);
-  }
+            initializeCropper(img);
+        }
+        reader.readAsDataURL(file);
+    }
 }
 
-document.getElementById("cropButton").addEventListener("click", function () {
-  const croppedCanvas = getCroppedCanvas();
-  processImage(croppedCanvas);
+document.getElementById('cropButton').addEventListener('click', function () {
+    const croppedCanvas = getCroppedCanvas();
+    processImage(croppedCanvas);
 });
